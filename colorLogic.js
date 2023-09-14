93df2219be237ad238c4d5bcf1de0ae3fbcf8865
Nicholas Nieminen Jönsson <nicholasnieminenjonsson@mbpsomtnicholas.home>
2023-09-14 16:10:00 +0200

    Lecture end

--- colorLogic.js
+++ colorLogic.js
@@ -1,1 +1,51 @@
-document.getElementById("my-colour-id").style.color = "red";
+function changeColor() {
+    document.getElementById("my-color-id").style.color = "green";
+}
+
+//Ska variabler!
+
+function variables() {
+    
+    var name = ""
+    var age = 0
+
+    name = "Benny "
+    age = 15
+
+
+    alert(name + age)
+    console.log(name + age);  // Shows up in INSPECTION WINDOW
+
+}
+
+function arrays() {
+    
+    var score = [100, 150, 250, 1000]
+
+    alert(score[0])
+    alert(score[3])
+
+    // Index
+    // Element
+    // [] == Array
+
+}
+
+function userLogin() {
+
+    var result = prompt("please input login credentials")
+
+    alert(result)
+
+}
+
+function loopExercise() {
+    
+    for (var index = 0; index < 10; index++) {
+        
+        alert(index)
+        
+    }
+
+}
+
